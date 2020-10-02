3ca13bff903d11b949521284b9d94a5d8f6b3d67
PabloMoreno2000 <pamta2000@gmail.com>
2020-10-01 20:47:11 -0500

    Added route to update a card

--- homeworks/CRUD_pokemon/routes/pokemon.js
+++ homeworks/CRUD_pokemon/routes/pokemon.js
@@ -51,4 +51,32 @@ router.get("/getAll", (req, res) => {
   res.json(cards);
 });
 
+router.put(
+  "/update/:id",
+  [
+    check("name", "Please insert a name for the card").exists(),
+    check("desct", "Please insert a description(desct) for the card").exists(),
+    check("type", "Please insert a type for this card").isIn(cardTypes),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    const { name, desct, type } = req.body;
+    let idRequested = req.params.id;
+    let card = cards[idRequested];
+
+    if (card) {
+      cards[idRequested] = { idRequested, name, desct, type };
+      res.json(cards[idRequested]);
+    } else {
+      return res.status(404).json({ msg: "Card not found" });
+    }
+  }
+);
+
 module.exports = router;
