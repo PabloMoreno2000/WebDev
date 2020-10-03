2ebfd180007a5954e2efee26dcdc65f9dac1e51c
PabloMoreno2000 <pamta2000@gmail.com>
2020-10-03 14:09:46 -0500

    Create pokemon card uses an external api

--- homeworks/CRUD_pokemon/routes/pokemon.js
+++ homeworks/CRUD_pokemon/routes/pokemon.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const axios = require("axios");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
 
 const cardTypes = ["pokemon", "item", "price", "element"];
 const cards = {};
@@ -11,16 +12,14 @@ const cards = {};
 // This is an id counter
 let id = 0;
 
-// @route
-// @desct
+// @desct Creates and returns a new card
 router.post(
   "/create",
   [
     check("name", "Please insert a name for the card").exists(),
-    check("desct", "Please insert a description(desct) for the card").exists(),
     check("type", "Please insert a type for this card").isIn(cardTypes),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({
@@ -28,15 +27,47 @@ router.post(
       });
     }
 
-    // Let's extract and then create the object to avoid saving undesired parameters
-    const { name, desct, type } = req.body;
+    const name = req.body.name;
+    const type = req.body.type;
+    let desct = req.body.desct;
+    // "desct" attribute is not needed just if type is pokemon
+    if (!desct && type != "pokemon") {
+      return res
+        .status(400)
+        .json({ msg: "desct needed por non-pokemon cards" });
+    }
+
+    // Check there's no other card with the same name
+    for (let i = 0; i < cards.length; i++) {
+      let card = cards[i];
+      if (card.name == name) {
+        return res.status(400).json({ msg: "Card name already exists" });
+      }
+    }
+
+    // The description of a pokemon card is the pokemon itself
+    if (type == "pokemon") {
+      try {
+        let resp = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        // Some pokemons have hundreds of moves, each with a lot of info, let's keep just some of them
+        let movesCount = Math.min(3, resp.data.moves.length);
+        resp.data.moves = resp.data.moves.slice(0, movesCount);
+        desct = resp.data;
+      } catch (error) {
+        return res.status(400).json({ msg: "Invalid pokemon name" });
+      }
+    }
+
     // Remember this is the same as {"name": name, "desct": desct...}
     cards[id] = { id, name, desct, type };
+    // Return the card
+    res.json(cards[id]);
     // Increase counter for next card
     id++;
   }
 );
 
+// @desct Returns a card by its id
 router.get("/get/:id", (req, res) => {
   let idRequested = req.params.id;
   let card = cards[idRequested];
@@ -47,10 +78,12 @@ router.get("/get/:id", (req, res) => {
   }
 });
 
+// @desct Returns all cards within the server
 router.get("/getAll", (req, res) => {
   res.json(cards);
 });
 
+// @desct Updates the information of an existing card
 router.put(
   "/update/:id",
   [
@@ -79,6 +112,7 @@ router.put(
   }
 );
 
+// @desct Deletes an existing card by its id
 router.delete("/delete/:id", (req, res) => {
   let idRequested = req.params.id;
   let card = cards[idRequested];
